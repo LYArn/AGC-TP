688224bd398e8f6673fe37fbac4711e2cb933474
Arnaud LY <arnaudly.pro@gmail.com>
2022-10-20 15:57:20 +0200

    Add files

--- agc/agc.py
+++ agc/agc.py
@@ -14,6 +14,7 @@
 """OTU clustering"""
 
 import argparse
+from ctypes import alignment
 import sys
 import os
 import gzip
@@ -23,6 +24,7 @@ from collections import Counter
 # https://github.com/briney/nwalign3
 # ftp://ftp.ncbi.nih.gov/blast/matrices/
 import nwalign3 as nw
+
 
 __author__ = "Your Name"
 __copyright__ = "Universite Paris Diderot"
@@ -53,44 +55,91 @@ def get_arguments():
       Returns: An object that contains the arguments
     """
     # Parsing arguments
-    parser = argparse.ArgumentParser(description=__doc__, usage=
-                                     "{0} -h"
+    parser = argparse.ArgumentParser(description=__doc__, usage="{0} -h"
                                      .format(sys.argv[0]))
-    parser.add_argument('-i', '-amplicon_file', dest='amplicon_file', type=isfile, required=True, 
+    parser.add_argument('-i', '-amplicon_file', dest='amplicon_file', type=isfile, required=True,
                         help="Amplicon is a compressed fasta file (.fasta.gz)")
-    parser.add_argument('-s', '-minseqlen', dest='minseqlen', type=int, default = 400,
-                        help="Minimum sequence length for dereplication (default 400)")
-    parser.add_argument('-m', '-mincount', dest='mincount', type=int, default = 10,
-                        help="Minimum count for dereplication  (default 10)")
-    parser.add_argument('-c', '-chunk_size', dest='chunk_size', type=int, default = 100,
+    parser.add_argument('-s', '-minseqlen', dest='minseqlen', type=int, default=200,
+                        help="Minimum sequence length for dereplication (default 200)")
+    parser.add_argument('-m', '-mincount', dest='mincount', type=int, default=3,
+                        help="Minimum count for dereplication  (default 3)")
+    parser.add_argument('-c', '-chunk_size', dest='chunk_size', type=int, default=100,
                         help="Chunk size for dereplication  (default 100)")
-    parser.add_argument('-k', '-kmer_size', dest='kmer_size', type=int, default = 8,
+    parser.add_argument('-k', '-kmer_size', dest='kmer_size', type=int, default=8,
                         help="kmer size for dereplication  (default 10)")
     parser.add_argument('-o', '-output_file', dest='output_file', type=str,
                         default="OTU.fasta", help="Output file")
     return parser.parse_args()
 
+
 def read_fasta(amplicon_file, minseqlen):
-    pass
+    seq = ""
+    with gzip.open(amplicon_file, "rt") as file:
+        for line in file:
+            if line[0] == ">":
+                if seq == "":
+                    continue
+                elif len(seq) >= minseqlen:
+                    yield seq
+                seq = ""
+                continue
+            seq += line.strip()
+    if len(seq) >= minseqlen:
+        yield seq
 
 
 def dereplication_fulllength(amplicon_file, minseqlen, mincount):
-    pass
+    occ = {}
+
+    for i in read_fasta(amplicon_file, minseqlen):
+        if i not in occ:
+            occ[i] = 1
+        else:
+            occ[i] += 1
+    
+    for k, v in sorted(occ.items(), key= lambda x: x[1], reverse = True):
+        if v >= mincount:
+            yield [k, v]
+
 
 def get_identity(alignment_list):
-    """Prend en une liste de séquences alignées au format ["SE-QUENCE1", "SE-QUENCE2"]
-    Retourne le pourcentage d'identite entre les deux."""
-    pass
+    """Prend en une liste de séquences alignées au format ["SE-QUENCE1", 
+    "SE-QUENCE2"] Retourne le pourcentage d'identite entre les deux."""
+    count = 0
+    for n in range(len(alignment_list[0])):
+        if alignment_list[0][n] == alignment_list[1][n]:
+            count += 1
+        
+    return count/len(alignment_list[0])*100
+        
 
-def abundance_greedy_clustering(amplicon_file, minseqlen, mincount, chunk_size, kmer_size):
-    pass
+def abundance_greedy_clustering(amplicon_file, minseqlen, mincount, chunk_size,
+kmer_size):
+    OTU_list = []
+    for k, v in dereplication_fulllength(amplicon_file, minseqlen, mincount):
+        if not OTU_list:
+            OTU_list.append([k, v])
+
+        for i in OTU_list:
+            alignment_list = nw.global_align(i[0], k, gap_open=-1, gap_extend=-1,
+            matrix=os.path.abspath(os.path.join(os.path.dirname(__file__),"MATCH")))
+            
+            if get_identity(alignment_list) > 97:
+                continue
+            OTU_list.append([k, v])
+            
+    return OTU_list
+
+        
 
 def write_OTU(OTU_list, output_file):
     pass
 
-#==============================================================
+# ==============================================================
 # Main program
-#==============================================================
+# ==============================================================
+
+
 def main():
     """
     Main program function
@@ -99,37 +148,45 @@ def main():
     args = get_arguments()
     # Votre programme ici
 
-#==============================================================
+# ==============================================================
 # Chimera removal section
-#==============================================================
+# ==============================================================
+
 
 def get_unique(ids):
     return {}.fromkeys(ids).keys()
 
-def common(lst1, lst2): 
+
+def common(lst1, lst2):
     return list(set(lst1) & set(lst2))
+
 
 def get_chunks(sequence, chunk_size):
     """Split sequences in a least 4 chunks
     """
     pass
 
+
 def cut_kmer(sequence, kmer_size):
     """Cut sequence into kmers"""
     pass
 
+
 def get_unique_kmer(kmer_dict, sequence, id_seq, kmer_size):
     pass
+
 
 def detect_chimera(perc_identity_matrix):
     pass
 
+
 def search_mates(kmer_dict, sequence, kmer_size):
     pass
+
 
 def chimera_removal(amplicon_file, minseqlen, mincount, chunk_size, kmer_size):
     pass
 
 
 if __name__ == '__main__':
-    main()
+    pass
