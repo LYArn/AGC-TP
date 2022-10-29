7609b29068f33abd06fc7f12f93039452251de77
Arnaud LY <arnaudly.pro@gmail.com>
2022-10-29 19:18:42 +0200

    final

--- agc/agc.py
+++ agc/agc.py
@@ -20,15 +20,14 @@ import os
 import gzip
 import statistics
 import textwrap
-from collections import Counter
 # https://github.com/briney/nwalign3
 # ftp://ftp.ncbi.nih.gov/blast/matrices/
 import nwalign3 as nw
 
 
-__author__ = "Your Name"
+__author__ = "Arnaud LY"
 __copyright__ = "Universite Paris Diderot"
-__credits__ = ["Your Name"]
+__credits__ = ["Arnaud LY"]
 __license__ = "GPL"
 __version__ = "1.0.0"
 __maintainer__ = "Your Name"
@@ -74,12 +73,12 @@ def get_arguments():
 
 def read_fasta(amplicon_file, minseqlen):
     seq = ""
-    with gzip.open(amplicon_file, "rt") as file:
+    with open(amplicon_file, "rt") as file:
         for line in file:
             if line[0] == ">":
                 if seq == "":
                     continue
-                elif len(seq) >= minseqlen:
+                if len(seq) >= minseqlen:
                     yield seq
                 seq = ""
                 continue
@@ -96,8 +95,8 @@ def dereplication_fulllength(amplicon_file, minseqlen, mincount):
             occ[i] = 1
         else:
             occ[i] += 1
-    
-    for k, v in sorted(occ.items(), key= lambda x: x[1], reverse = True):
+
+    for k, v in sorted(occ.items(), key=lambda x: x[1], reverse=True):
         if v >= mincount:
             yield [k, v]
 
@@ -109,32 +108,33 @@ def get_identity(alignment_list):
     for n in range(len(alignment_list[0])):
         if alignment_list[0][n] == alignment_list[1][n]:
             count += 1
-        
+
     return count/len(alignment_list[0])*100
-        
+
 
 def abundance_greedy_clustering(amplicon_file, minseqlen, mincount):
-    OTU_list = []
+    otu_list = []
+
     for k, v in dereplication_fulllength(amplicon_file, minseqlen, mincount):
-        if not OTU_list:
-            OTU_list.append([k, v])
+        if not otu_list:
+            otu_list.append([k, v])
 
-        for i in OTU_list:
-            alignment_list = nw.global_align(i[0], k, gap_open=-1, gap_extend=-1,
-            matrix=os.path.abspath(os.path.join(os.path.dirname(__file__),"MATCH")))
-            
-            if get_identity(alignment_list) > 97:
-                continue
-            OTU_list.append([k, v])
-            
-    return OTU_list
+        for i in otu_list:
+            alignment_list = nw.global_align(k, i[0], gap_open=-1, gap_extend=-1,
+                                             matrix=os.path.abspath(os.path.join(os.path.dirname(__file__), "MATCH")))
+
+            if get_identity(list(alignment_list)) >= 97:
+                break
+            otu_list.append([k, v])
+
+    return otu_list
 
 
-def write_OTU(OTU_list, output_file):
+def write_OTU(otu_list, output_file):
     with open(output_file, 'w') as f:
-        for i in range(len(OTU_list)):
-            f.write(f">OTU_{i+1} occurrence:{OTU_list[i][1]}\n")
-            f.write(textwrap.fill(OTU_list[i][0], width=80))
+        for i in range(len(otu_list)):
+            f.write(f">OTU_{i+1} occurrence:{otu_list[i][1]}\n")
+            f.write(textwrap.fill(otu_list[i][0], width=80))
             f.write("\n")
 
 
@@ -151,8 +151,9 @@ def main():
     args = get_arguments()
 
     # Generate list of Operational Taxonomic Unit (OTU)
-    otu_list = abundance_greedy_clustering(args.amplicon_file, args.minseqlen, args.mincount)
-    
+    otu_list = abundance_greedy_clustering(
+        args.amplicon_file, args.minseqlen, args.mincount)
+
     # Write OTU list in file
     write_OTU(otu_list, args.output_file)
 
@@ -161,39 +162,39 @@ def main():
 # ==============================================================
 
 
-def get_unique(ids):
-    return {}.fromkeys(ids).keys()
+# def get_unique(ids):
+#     return {}.fromkeys(ids).keys()
 
 
-def common(lst1, lst2):
-    return list(set(lst1) & set(lst2))
+# def common(lst1, lst2):
+#     return list(set(lst1) & set(lst2))
 
 
-def get_chunks(sequence, chunk_size):
-    """Split sequences in a least 4 chunks
-    """
-    pass
+# def get_chunks(sequence, chunk_size):
+#     """Split sequences in a least 4 chunks
+#     """
+#     pass
 
 
-def cut_kmer(sequence, kmer_size):
-    """Cut sequence into kmers"""
-    pass
+# def cut_kmer(sequence, kmer_size):
+#     """Cut sequence into kmers"""
+#     pass
 
 
-def get_unique_kmer(kmer_dict, sequence, id_seq, kmer_size):
-    pass
+# def get_unique_kmer(kmer_dict, sequence, id_seq, kmer_size):
+#     pass
 
 
-def detect_chimera(perc_identity_matrix):
-    pass
+# def detect_chimera(perc_identity_matrix):
+#     pass
 
 
-def search_mates(kmer_dict, sequence, kmer_size):
-    pass
+# def search_mates(kmer_dict, sequence, kmer_size):
+#     pass
 
 
-def chimera_removal(amplicon_file, minseqlen, mincount, chunk_size, kmer_size):
-    pass
+# def chimera_removal(amplicon_file, minseqlen, mincount, chunk_size, kmer_size):
+#     pass
 
 
 if __name__ == '__main__':
